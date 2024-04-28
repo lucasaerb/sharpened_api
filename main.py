6c009006ad992ea01d664e833d9433dc25d906a2
lucasaerb <lucasaerb@gmail.com>
2024-04-28 19:29:35 -0400

    fix files

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import uvicorn
 
 load_dotenv()
 
-PORT = int(os.getenv("PORT", 8000))
-HOST = '0.0.0.0'
+PORT = int(os.getenv("PORT", 3000))
+HOST = 'localhost'
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host=HOST, port=PORT, reload=True)
