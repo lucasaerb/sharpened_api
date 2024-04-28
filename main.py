32f5141800307e9b452507ecb60b55949f690ee2
lucasaerb <lucasaerb@gmail.com>
2024-04-28 19:50:39 -0400

    hello

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-from dotenv import load_dotenv
 import os
 import uvicorn
 
-load_dotenv()
 
 
-PORT = int(os.getenv("PORT", 3000))
-HOST = 'localhost'
+PORT = int(os.getenv("PORT", 8000))
+HOST = '0.0.0.0'
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host=HOST, port=PORT, reload=True)
+    uvicorn.run("app.api:app", host=HOST, port=PORT, reload=True)
