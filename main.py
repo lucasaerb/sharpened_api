54f4f8bdd14427624be53f6b56176b1ab5bedc86
lucasaerb <lucasaerb@gmail.com>
2024-05-04 20:08:07 -0400

    new path

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ PORT = int(os.getenv("PORT", 8000))
 HOST = '0.0.0.0'
 
 if __name__ == "__main__":
-    uvicorn.run("app.api:app", host=HOST, port=PORT, reload=True)
+    uvicorn.run("api.index:app", host=HOST, port=PORT, reload=True)
