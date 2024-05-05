b305083a193130dcf34aeea18708e350669211e2
lucasaerb <lucasaerb@gmail.com>
2024-05-04 20:24:28 -0400

    working?

--- api/index.py
+++ api/index.py
@@ -9,17 +9,6 @@ origins = [
 ]
 
 
-from http.server import BaseHTTPRequestHandler
- 
-class handler(BaseHTTPRequestHandler):
- 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
-        return
-
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
